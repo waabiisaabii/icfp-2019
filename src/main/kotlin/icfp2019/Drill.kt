79cb724f7883dcdc2fd3be951e660d95d4436144
Akshay-Iyangar <Akshay.Iyangar@asu.edu>
2019-06-22 02:03:17 -0700

    Add drill count (#63)
    * Add gamestate.
    
    * fix spotless.
    
    * adding Drill count.
    
    * Add changes for drill in all direction.
    
    * fix spotless
    
    * fix `E`

---
+++ src/main/kotlin/icfp2019/Drill.kt
@@ -0,0 +1,45 @@
+package icfp2019
+
+fun buildDrillRequiredFromEachNode(problem: Problem): Array<Array<Array<DrillState>>> {
+
+    // filter { !it.isObstacle }
+    val currentGrid = problem.map
+    return currentGrid.mapIndexed { x, row ->
+        row.mapIndexed { y, _ ->
+            if (!currentGrid[x][y].isObstacle) {
+                arrayOf(DrillState(Direction.E, -1)) // -1
+            } else {
+                val rightDir =
+                    (x until problem.size.x).map {
+                        currentGrid[it][y]
+                    }
+                        .takeWhile { it.isObstacle }
+                        .count()
+
+                val leftDir = (x downTo 0).map {
+                    currentGrid[it][y]
+                }
+                    .takeWhile { it.isObstacle }
+                    .count()
+
+                val upDir = (y until problem.size.y).map {
+                    currentGrid[x][it]
+                }
+                    .takeWhile { it.isObstacle }
+                    .count()
+
+                val downDir = (y downTo 0).map {
+                    currentGrid[x][it]
+                }
+                    .takeWhile { it.isObstacle }
+                    .count()
+
+                arrayOf(DrillState(Direction.R, rightDir),
+                    DrillState(Direction.L, leftDir),
+                    DrillState(Direction.U, upDir),
+                    DrillState(Direction.D, downDir)
+                    )
+            }
+        }.toTypedArray()
+    }.toTypedArray()
+}
---
+++ src/main/kotlin/icfp2019/DrillState.kt
@@ -0,0 +1,3 @@
+package icfp2019
+
+data class DrillState(val direction: Direction, val value: Int)
