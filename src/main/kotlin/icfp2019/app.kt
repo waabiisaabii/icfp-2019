fc68341055d683016cd99becd17f736e9bd5a77e
Akshay-Iyangar <Akshay.Iyangar@asu.edu>
2019-06-21 20:34:46 -0700

    add builder for robotstate (#47)
    * add builder for robotstate
    
    * making some more robotState.
    
    * git rid of unused imports.
    
    * fix spotless.

--- src/main/kotlin/icfp2019/app.kt
+++ src/main/kotlin/icfp2019/app.kt
@@ -84,6 +84,20 @@ enum class Actions {
     W, S, A, D, Z, E, Q, B, F, L
 }
 
+data class RobotState(
+    val robotId: RobotId,
+    val currentPosition: Point,
+    val orientation: Orientation = Orientation.Up,
+    val remainingFastWheelTime: Int? = null,
+    val remainingDrillTime: Int? = null
+)
+
+data class RobotId(val id: Int)
+
+enum class Orientation {
+    Up, Down, Left, Right
+}
+
 data class Solution(val problemId: ProblemId, val actions: List<Actions>)
 
 fun solve(problem: Problem): Solution {
