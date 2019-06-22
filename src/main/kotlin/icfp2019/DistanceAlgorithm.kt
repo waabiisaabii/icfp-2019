f52d87a6225e9daa5036561140d2125fca1d343c
teemo <teemobean@gmail.com>
2019-06-21 20:39:08 -0700

    Simple distance algorithm (#49)
    * Simple distance algorithm
    
    * Spotless fixes

---
+++ src/main/kotlin/icfp2019/DistanceAlgorithm.kt
@@ -0,0 +1,21 @@
+package icfp2019
+
+// Given a map, return a map of weights where the weight indicates the distance
+// the square is from the walls. We will sum the distance to close horizontal and vertical walls.
+
+fun applyDistanceAlgorithm(map: Array<Array<Node>>): List<List<Int>> {
+    val maxX = map.size
+    val maxY = map[0].size
+    val ret = map.mapIndexed { x, subArray ->
+        subArray.mapIndexed { y, node ->
+            if (node.isObstacle) {
+                -1
+            } else {
+                val weightX = if (x < maxX / 2) x + 1 else maxX - x
+                val weightY = if (y < maxY / 2) y + 1 else maxY - y
+                weightX + weightY
+            }
+        }
+    }
+    return ret
+}
