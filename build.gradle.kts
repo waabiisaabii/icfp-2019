41f10fa485a79b5635b2bbf06345a15ea32235e2
Nathan Howell <nathan.d.howell@gmail.com>
2019-06-22 19:10:37 -0700

    We want a robot shape (#113)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(platform("org.junit:junit-bom:5.4.0"))
 
+    implementation("ca.umontreal.iro.simul:ssj:3.3.1")
     implementation("com.google.guava:guava:28.0-jre")
     implementation("org.jgrapht:jgrapht-core:1.3.0")
     implementation("org.junit.jupiter:junit-jupiter")
---
+++ src/main/kotlin/icfp2019/model/RobotShape.kt
@@ -0,0 +1,5 @@
+package icfp2019.model
+
+import umontreal.ssj.util.BitMatrix
+
+data class RobotShape(val bitMatrix: BitMatrix)
