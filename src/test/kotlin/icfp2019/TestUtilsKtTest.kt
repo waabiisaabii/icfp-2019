3fde88ee6389ec3240e40ebbd600e17a3982dca3
Yachen Lin <yachenl1@alumni.cmu.edu>
2019-06-23 06:19:30 -0700

    format crap

--- src/test/kotlin/icfp2019/TestUtilsKtTest.kt
+++ src/test/kotlin/icfp2019/TestUtilsKtTest.kt
@@ -12,23 +12,23 @@ internal class TestUtilsKtTest {
                         XXXXXXXX
                     """.toProblem()
         Assertions.assertTrue(
-            problem.get(1, 1).isWrapped.not()
-                    && problem.get(1, 1).isObstacle.not()
+            problem.get(1, 1).isWrapped.not() &&
+                    problem.get(1, 1).isObstacle.not()
         )
 
         Assertions.assertTrue(
-            problem.get(2, 1).isWrapped.not()
-                    && problem.get(2, 1).isObstacle.not()
+            problem.get(2, 1).isWrapped.not() &&
+                    problem.get(2, 1).isObstacle.not()
         )
 
         Assertions.assertTrue(
-            problem.get(7, 2).isWrapped.not()
-                    && problem.get(7, 2).isObstacle
+            problem.get(7, 2).isWrapped.not() &&
+                    problem.get(7, 2).isObstacle
         )
 
         Assertions.assertTrue(
-            problem.get(6, 1).isWrapped
-                    && problem.get(6, 1).isObstacle.not()
+            problem.get(6, 1).isWrapped &&
+                    problem.get(6, 1).isObstacle.not()
         )
     }
 }
--- src/test/kotlin/icfp2019/analyzers/DFSAnalyzerTest.kt
+++ src/test/kotlin/icfp2019/analyzers/DFSAnalyzerTest.kt
@@ -8,78 +8,78 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class DFSAnalyzerTest {
-  @Test
-  fun `get rid of being stuck`() {
-    val problem = """
-                        XXXXXXXX
-                        XXXXwX.X
-                        XXXXwX.X
-                        Xw@wwX.X
-                        XXXX...X
-                        XXXXXXXX
-                    """.toProblem()
+    @Test
+    fun `get rid of being stuck`() {
+        val problem = """
+                            XXXXXXXX
+                            XXXXwX.X
+                            XXXXwX.X
+                            Xw@wwX.X
+                            XXXX...X
+                            XXXXXXXX
+                        """.toProblem()
 
-    val gameBoard = GameBoard(problem)
-    val startingState = GameState
-        .gameStateOf(problem)
+        val gameBoard = GameBoard(problem)
+        val startingState = GameState
+            .gameStateOf(problem)
 
-    val actions = DFSAnalyzer
-        .analyze(gameBoard)
-        .invoke(startingState)
+        val actions = DFSAnalyzer
+            .analyze(gameBoard)
+            .invoke(startingState)
 
-    Assertions.assertEquals(
-        Action.MoveRight,
-        actions.first()
-    )
-  }
+        Assertions.assertEquals(
+            Action.MoveRight,
+            actions.first()
+        )
+    }
 
-  @Test
-  fun `get rid of being stuck 3`() {
-    val problem = """
-                        XXXXXXXX
-                        XXXXwX.X
-                        XXXXwX.X
-                        Xww@wX.X
-                        XXXX...X
-                        XXXXXXXX
-                    """.toProblem()
+    @Test
+    fun `get rid of being stuck 3`() {
+        val problem = """
+                            XXXXXXXX
+                            XXXXwX.X
+                            XXXXwX.X
+                            Xww@wX.X
+                            XXXX...X
+                            XXXXXXXX
+                        """.toProblem()
 
-    val gameBoard = GameBoard(problem)
-    val startingState = GameState
-        .gameStateOf(problem)
+        val gameBoard = GameBoard(problem)
+        val startingState = GameState
+            .gameStateOf(problem)
 
-    val actions = DFSAnalyzer
-        .analyze(gameBoard)
-        .invoke(startingState)
+        val actions = DFSAnalyzer
+            .analyze(gameBoard)
+            .invoke(startingState)
 
-    Assertions.assertEquals(
-        Action.MoveRight,
-        actions.first()
-    )
-  }
+        Assertions.assertEquals(
+            Action.MoveRight,
+            actions.first()
+        )
+    }
 
-  @Test
-  fun `verify dfs movement`() {
-    val problem = """
-                        XXXXXXXX
-                        XXXXwX.X
-                        XXXXwX.X
-                        Xwww@X.X
-                        XXXX...X
-                        XXXXXXXX
-                    """.toProblem()
+    @Test
+    fun `verify dfs movement`() {
+        val problem = """
+                            XXXXXXXX
+                            XXXXwX.X
+                            XXXXwX.X
+                            Xwww@X.X
+                            XXXX...X
+                            XXXXXXXX
+                        """.toProblem()
 
-    val gameBoard = GameBoard(problem)
-    val startingState = GameState
-        .gameStateOf(problem)
+        val gameBoard = GameBoard(problem)
+        val startingState = GameState
+            .gameStateOf(problem)
 
-    val actions = DFSAnalyzer
-        .analyze(gameBoard)
-        .invoke(startingState)
+        val actions = DFSAnalyzer
+            .analyze(gameBoard)
+            .invoke(startingState)
 
-    Assertions.assertEquals(
-        Action.MoveDown,
-        actions.first()
-    )
-  }
+        Assertions.assertEquals(
+            Action.MoveDown,
+            actions.first()
+        )
+    }
 }
