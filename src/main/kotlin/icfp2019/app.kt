8f5d7e5f02d09716bda1a09c75f04ce94c06e5d2
John Kercheval <kercheval@gmail.com>
2019-06-23 15:22:53 -0700

    Create log files (#146)

--- src/main/kotlin/icfp2019/app.kt
+++ src/main/kotlin/icfp2019/app.kt
@@ -4,16 +4,23 @@ import icfp2019.model.*
 import icfp2019.strategies.DFSStrategy
 import java.io.File
 import java.nio.file.Paths
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     val path = Paths.get(if (args.isNotEmpty()) args[0] else "./problems").toAbsolutePath()
     path.toFile().walk().forEach {
         if (it.isFile && it.extension.equals("desc")) {
-            println("Running " + it.name)
-            val problem = parseDesc(it.readText(), "Test")
-            val solution = brain(problem, listOf(DFSStrategy), 1)
-            File(it.parent, "${it.nameWithoutExtension}.sol").writeBytes(solution.toString().toByteArray())
-            println(solution.summary())
+            var summary = "Invalid Problem"
+            val timeElapsed = measureTimeMillis {
+                print("Running ${it.name}... ")
+                val problem = parseDesc(it.readText(), it.name)
+                val solution = brain(problem, listOf(DFSStrategy), 1)
+                File(it.parent, "${it.nameWithoutExtension}.sol").writeText(solution.toString())
+                summary = solution.summary()
+            }
+            val logSummary = "$summary in ${timeElapsed}ms\n"
+            print(logSummary)
+            File(it.parent, "${it.nameWithoutExtension}.log").appendText(logSummary)
         }
     }
 }
