98b4929b7d8c68483b78bb4118c84e83ed879557
Yachen Lin <yachenl1@alumni.cmu.edu>
2019-06-23 05:54:19 -0700

    wrap up dfs

--- src/main/kotlin/icfp2019/strategies/DFSStrategy.kt
+++ src/main/kotlin/icfp2019/strategies/DFSStrategy.kt
@@ -5,8 +5,11 @@ import icfp2019.analyzers.ShortestPathUsingFlyodWarshall
 import icfp2019.core.DistanceEstimate
 import icfp2019.core.Proposal
 import icfp2019.core.Strategy
-import icfp2019.model.*
+import icfp2019.model.GameBoard
+import icfp2019.model.GameState
+import icfp2019.model.Node
 import org.jgrapht.Graph
+import org.jgrapht.GraphPath
 import org.jgrapht.graph.AsSubgraph
 import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.traverse.DepthFirstIterator
@@ -21,23 +24,36 @@ object DFSStrategy : Strategy {
             val currentPoint = gameState.robotState.values.first().currentPosition
             val currentNode = gameState.get(currentPoint)
 
-            val unwrappedGraph = AsSubgraph(graph, graph.vertexSet().filter { it.isWrapped.not() }.plus(currentNode).toSet())
+            val unwrappedGraph =
+                AsSubgraph(graph, graph.vertexSet().filter { it.isWrapped.not() }.plus(currentNode).toSet())
 
             val it: GraphIterator<Node, DefaultEdge> = DepthFirstIterator(unwrappedGraph)
-            if (!it.hasNext()) {
+
+            val neighbors = currentNode.point.neighbors().map { gameState.get(it) }
+            if (neighbors.any {
+                    it.isWrapped.not() && it.isObstacle.not()
+                }) {
+                val neighbor = it.next().point
+                Proposal(
+                    DistanceEstimate(0),
+                    currentPoint.actionToGetToNeighbor(neighbor)
+                )
+            } else {
                 val analyze = ShortestPathUsingFlyodWarshall.analyze(map)
                 val shortestPathAlgorithm = analyze(gameState)
 
-                val pathToClosestNode = unwrappedGraph.vertexSet()
+                val pathToClosestNode: GraphPath<Node, DefaultEdge> = unwrappedGraph.vertexSet()
+                    .filter { it.point != currentNode.point }
                     .filter { it.isWrapped.not() }
                     .map { shortestPathAlgorithm.getPath(currentNode, it) }
-                    .minBy { it.length }
+                    .minBy { it.length }!!
 
-                val nextNode = pathToClosestNode!!.vertexList[0]
-                Proposal(DistanceEstimate(0), currentPoint.actionToGetToNeighbor(nextNode.point))
-            } else {
-                val neighbor = it.next().point
-                Proposal(DistanceEstimate(0), currentPoint.actionToGetToNeighbor(neighbor))
+                // pathToClosestNode.vertexList[0] is `currentNode`
+                val nextNode = pathToClosestNode.vertexList[1]
+                Proposal(
+                    DistanceEstimate(0),
+                    currentPoint.actionToGetToNeighbor(nextNode.point)
+                )
             }
         }
     }
---
+++ src/test/kotlin/icfp2019/analyzers/DFSAnalyzerTest.kt
@@ -0,0 +1,85 @@
+package icfp2019.analyzers
+
+import icfp2019.model.Action
+import icfp2019.model.GameBoard
+import icfp2019.model.GameState
+import icfp2019.toProblem
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DFSAnalyzerTest {
+  @Test
+  fun `get rid of being stuck`() {
+    val problem = """
+                        XXXXXXXX
+                        XXXXwX.X
+                        XXXXwX.X
+                        Xw@wwX.X
+                        XXXX...X
+                        XXXXXXXX
+                    """.toProblem()
+
+    val gameBoard = GameBoard(problem)
+    val startingState = GameState
+        .gameStateOf(problem)
+
+    val actions = DFSAnalyzer
+        .analyze(gameBoard)
+        .invoke(startingState)
+
+    Assertions.assertEquals(
+        Action.MoveRight,
+        actions.first()
+    )
+  }
+
+  @Test
+  fun `get rid of being stuck 3`() {
+    val problem = """
+                        XXXXXXXX
+                        XXXXwX.X
+                        XXXXwX.X
+                        Xww@wX.X
+                        XXXX...X
+                        XXXXXXXX
+                    """.toProblem()
+
+    val gameBoard = GameBoard(problem)
+    val startingState = GameState
+        .gameStateOf(problem)
+
+    val actions = DFSAnalyzer
+        .analyze(gameBoard)
+        .invoke(startingState)
+
+    Assertions.assertEquals(
+        Action.MoveRight,
+        actions.first()
+    )
+  }
+
+  @Test
+  fun `verify dfs movement`() {
+    val problem = """
+                        XXXXXXXX
+                        XXXXwX.X
+                        XXXXwX.X
+                        Xwww@X.X
+                        XXXX...X
+                        XXXXXXXX
+                    """.toProblem()
+
+    val gameBoard = GameBoard(problem)
+    val startingState = GameState
+        .gameStateOf(problem)
+
+    val actions = DFSAnalyzer
+        .analyze(gameBoard)
+        .invoke(startingState)
+
+    Assertions.assertEquals(
+        Action.MoveDown,
+        actions.first()
+    )
+  }
+}
