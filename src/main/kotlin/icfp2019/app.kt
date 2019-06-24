bf3eb07b1a44e6ec8a74b2b67952a6a6895e2ccb
John Kercheval <kercheval@gmail.com>
2019-06-23 17:15:21 -0700

    Use BFS strategy by default for main (#151)
    * Use BFS strategy by default for main
    
    * Spotless

--- src/main/kotlin/icfp2019/app.kt
+++ src/main/kotlin/icfp2019/app.kt
@@ -1,7 +1,7 @@
 package icfp2019
 
 import icfp2019.model.*
-import icfp2019.strategies.DFSStrategy
+import icfp2019.strategies.BFSStrategy
 import java.io.File
 import java.nio.file.Paths
 import kotlin.system.measureTimeMillis
@@ -11,7 +11,7 @@ private fun computeSolution(file: File): Pair<Solution?, Long> {
     val timeElapsed = measureTimeMillis {
         print("Running ${file.name}... ")
         val problem = parseDesc(file.readText(), file.name)
-        brain(problem, listOf(DFSStrategy), 1).forEach { partialSolution ->
+        brain(problem, listOf(BFSStrategy), 1).forEach { partialSolution ->
             solution = partialSolution
             File(file.parentFile, "${file.nameWithoutExtension}.sol-partial").writeText(solution.toString())
         }
